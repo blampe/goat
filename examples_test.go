b3b3ab7e3a60101ace48107998860c58b94eb9da
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2022-02-20 12:44:36 +0100

    Improve the example tests

--- examples_test.go
+++ examples_test.go
@@ -2,6 +2,7 @@ package goat
 
 import (
 	"bytes"
+	"flag"
 	"io"
 	"io/ioutil"
 	"os"
@@ -12,61 +13,106 @@ import (
 	qt "github.com/frankban/quicktest"
 )
 
-const basePath string = "examples"
-
-func getInOut(t testing.TB, fileName string) (io.Reader, io.Writer) {
-	sourceName := filepath.Join(basePath, fileName)
-	svgName := filepath.Join(basePath, strings.TrimSuffix(fileName, filepath.Ext(fileName))+".svg")
-
-	in, err := os.Open(sourceName)
-	if err != nil {
-		t.Error(err)
-		return nil, nil
-	}
-
-	out, err := os.Create(svgName)
-	if err != nil {
-		t.Error(err)
-		return nil, nil
-	}
-
-	return in, out
-}
+var write = flag.Bool("write", false, "write examples to disk")
 
 func TestExamplesStableOutput(t *testing.T) {
 	c := qt.New(t)
 
 	var previous string
-	for i := 0; i < 2; i++ {
-		in, err := os.Open(filepath.Join(basePath, "circuits.svg"))
+	for i := 0; i < 3; i++ {
+		in, err := os.Open(filepath.Join(basePath, "circuits.txt"))
 		if err != nil {
 			t.Fatal(err)
 		}
 		var out bytes.Buffer
 		BuildAndWriteSVG(in, &out)
+		in.Close()
 		if i > 0 && previous != out.String() {
 			c.Fail()
 		}
 		previous = out.String()
+
 	}
 }
 
 func TestExamples(t *testing.T) {
-	fileInfos, err := ioutil.ReadDir(basePath)
-	if err != nil {
-		t.Error(err)
-	}
+	c := qt.New(t)
 
-	for _, fileInfo := range fileInfos {
-		in, out := getInOut(t, fileInfo.Name())
+	filenames, err := filepath.Glob(filepath.Join(basePath, "*.txt"))
+	c.Assert(err, qt.IsNil)
+
+	var buff *bytes.Buffer
+
+	for _, name := range filenames {
+		in := getIn(name)
+		var out io.WriteCloser
+		if *write {
+			out = getOut(name)
+		} else {
+			if buff == nil {
+				buff = &bytes.Buffer{}
+			} else {
+				buff.Reset()
+			}
+			out = struct {
+				io.Writer
+				io.Closer
+			}{
+				buff,
+				io.NopCloser(nil),
+			}
+		}
+
 		BuildAndWriteSVG(in, out)
+		in.Close()
+		out.Close()
+
+		if buff != nil {
+			golden := getOutString(name)
+			if buff.String() != golden {
+				c.Log(buff.Len(), len(golden))
+				c.Fatalf("Content mismatch for %s", name)
+			}
+			in.Close()
+			out.Close()
+		}
 	}
 }
 
 func BenchmarkComplicated(b *testing.B) {
-	in, out := getInOut(b, "complicated.txt")
+	in := getIn(filepath.FromSlash("examples/complicated.txt"))
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		BuildAndWriteSVG(in, out)
+		BuildAndWriteSVG(in, io.Discard)
 	}
+}
+
+const basePath string = "examples"
+
+func getIn(filename string) io.ReadCloser {
+	in, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	return in
+}
+
+func getOut(filename string) io.WriteCloser {
+	out, err := os.Create(toSVGFilename(filename))
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
+
+func getOutString(filename string) string {
+	b, err := ioutil.ReadFile(toSVGFilename(filename))
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+
+func toSVGFilename(filename string) string {
+	return strings.TrimSuffix(filename, filepath.Ext(filename)) + ".svg"
 }
