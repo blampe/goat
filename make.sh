2eb3e20933ad54ec2db1ed3029a54e561b636099
dmullis <dmullis>
2023-08-02 18:01:30 -0700

    Document workaround for #23: Safari-specific limit to CSS property inheritance
    Fix #20 and #21.

--- make.sh
+++ make.sh
@@ -3,8 +3,9 @@
 # Run all tests, and all pre-compilation build steps.
 # Certain output files should be committed to the SCM archive.
 #
-# Recall that 'go get ...' performs compilation-proper for go, within
-# the user's local environment.
+# Recall that an end-user eventually installing with 'go get ...'
+# will trigger a compilation from source within the local environment.
+#  XX  Give this file a more descriptive name.
 
 set -e
 set -x
@@ -19,10 +20,14 @@ usage () {
 # Define colors for SVG ~foreground~ seen on Github front page.
 svg_color_dark_scheme="#EEF"
 svg_color_light_scheme="#011"
+github_blue_color="#2F81F7"
 
-GOMOD=$(go env GOMOD)
-from_username=${GOMOD##*github.com/}
-githubuser=${from_username%%/*}
+# GOMOD=$(go env GOMOD)
+# from_username=${GOMOD##*github.com/}
+# githubuser=${from_username%%/*}
+#
+# X  Is it acceptable to push to a PR branch files that refer to the owner's main branch?
+githubuser=blampe
 
 TEST_ARGS=
 
@@ -49,8 +54,18 @@ tmpl_expand () {
 # this script with "TEST_ARGS=-write" first on the command line.
 # X  Results are used as "golden" standard for GitHub-side regression tests --
 #    so arguments here must not conflict with those in "test.yml".
+#   XX  How to share a single arg list shared between the two i.e. "DRY"?
 go test -run . -v \
    ${TEST_ARGS}
+
+# Illustrate a workaround for lack of support in certain browsers e.g. Safari for
+# inheritance of CSS property 'color-scheme' from <img> elements downward to nested
+# <svg> elements.
+#  - https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
+go run ./cmd/goat <examples/trees.txt \
+   -svg-color-dark-scheme ${github_blue_color} \
+   -svg-color-light-scheme ${github_blue_color} \
+   >trees.mid-blue.svg
 
 # build README.md
 tmpl_expand <README.md.tmpl >README.md \
